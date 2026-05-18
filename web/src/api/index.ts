f4d0ec94c351b444373f86f2dc47fcd2fb865d5a
Justin-Marian POPESCU <pmarianjustin@gmail.com>
2026-05-18 22:56:49 +0300

    update(web-api): update frontend API client

---
+++ web/src/api/index.ts
@@ -0,0 +1,14 @@
+export { api } from "./client";
+export { ApiError } from "./errors";
+export type {
+    ConfigResponse,
+    DatasetInfo,
+    DatasetsResponse,
+    HealthResponse,
+    Hit,
+    RagRequest,
+    RagResponse,
+    RetrieveRequest,
+    RetrieveResponse,
+    RetrieverName
+} from "./types";
